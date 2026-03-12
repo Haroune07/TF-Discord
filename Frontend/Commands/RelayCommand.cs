fe9e3ec9578c1e056b8ad32b7c5ac3125bb44bb9
HBelhachani <2471001@cegepmv.ca>
2026-03-11 22:11:26 -0400

    ajout du Patron Commande

---
+++ Frontend/Commands/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Windows.Input;
+
+namespace Frontend.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+    }
+}
