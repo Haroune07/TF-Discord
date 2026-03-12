2f22e760d3f415e650d3486093039c78488919d9
HBelhachani <2471001@cegepmv.ca>
2026-03-11 22:08:54 -0400

    implémenter Login + Register pour faire le pont entre backend et frontend

--- Frontend/Services/ApiService.cs
+++ Frontend/Services/ApiService.cs
@@ -1,6 +1,7 @@
 ﻿using Shared.DTOs.Auth;
 using System.Net.Http;
 using System.Net.Http.Json;
+using Shared.Constants;
 
 namespace Frontend.Services
 {
@@ -9,18 +10,24 @@ namespace Frontend.Services
 
         private readonly HttpClient _client;
 
-        public ApiService(string baseUrl)
+        public ApiService()
         {
-            _client = new()
-            {
-                BaseAddress = new Uri(baseUrl)
-            };
+            _client = new();
+            _client.BaseAddress = new Uri(Ports.SERVER_LISTEN_URL);
         }
 
-        async Task<AuthResponse> PostRegisterResponse(RegisterRequest req)
+        public async Task<AuthResponse> RegisterUserAsync(RegisterRequest req)
         {
 
-            var res = await _client.PostAsJsonAsync("api/auth/register", req);
+            var res = await _client.PostAsJsonAsync(Routes.RegisterRoute, req);
+
+            return (await res.Content.ReadFromJsonAsync<AuthResponse>())!;
+        }
+
+        public async Task<AuthResponse> LoginUserAsync(LoginRequest req)
+        {
+            var res = await _client.PostAsJsonAsync(Routes.LoginRoute, req);
+
             return (await res.Content.ReadFromJsonAsync<AuthResponse>())!;
         }
 
