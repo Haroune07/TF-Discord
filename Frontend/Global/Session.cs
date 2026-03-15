b5109af1e5eb961f79d435b31b13c12a69ccd5d8
HBelhachani <2471001@cegepmv.ca>
2026-03-15 17:09:45 -0400

    stockage de l'utilisateur actuel dans un singleton

---
+++ Frontend/Global/Session.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Shared.DTOs;
+
+namespace Frontend.Global
+{
+    internal class Session
+    {
+        private static Session? _session;
+        public static Session Current
+        {
+            get
+            {
+                if (_session == null)
+                {
+                    _session = new();
+                }
+
+                return _session;
+            }
+        } 
+
+        private Session() { }
+        public UserDTO? User { get; private set; }
+
+        public void Login(UserDTO user)
+        {
+            User = user;
+        }
+
+        public void Logout()
+        {
+            User = null;
+        }
+
+        public bool IsLoggedIn => User != null;
+
+    }
+}
