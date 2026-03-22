4c7ec626f6d29f82bcaa1ac5e14691a159bc8689
Carlof130 <2380179@cegepmv.ca>
2026-03-22 19:34:04 -0400

    Déconnexion

--- Frontend/ViewModels/HomeViewModel.cs
+++ Frontend/ViewModels/HomeViewModel.cs
@@ -1,18 +1,24 @@
-﻿using Frontend.Global;
+﻿using Frontend.Commands;
+using Frontend.Global;
 using Frontend.ViewModels.Base;
 using Shared.DTOs;
+using System.Diagnostics;
+using System.Windows.Input;
 namespace Frontend.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
-        private MainViewModel _main;
+        private MainViewModel? _main;
 
         public UserDTO? User { private set; get; } = Session.Current.User;
         public string OnlineStatus => User.IsOnline ? "Online" : "Offline";
 
+        public ICommand? GoToLoginCommand { get;}
+
         public HomeViewModel(MainViewModel main)
         {
             _main = main;
+            GoToLoginCommand = new RelayCommand(() => main.CurrentViewModel = new LoginViewModel(_main), () => true);
         }
     }
 }
