1c985f01442faed7601a380e6162b3c70f882565
Salah <2169461@cegepmv.ca>
2026-03-25 21:02:18 -0400

    Validation du mdp et de Username

--- Shared/Constants/Messages.cs
+++ Shared/Constants/Messages.cs
@@ -13,6 +13,8 @@ namespace Shared.Constants
         public const string UserNameAlreadyExists = "Ce nom d'utilisateur existe déjà.";
         public const string InvalidUsernameOrPassowrd = "Nom d'utilisateur ou mot de passe Invalide.";
         public const string LoginSuccess = "Login avec succès.";
+        public const string InvalidPasswordLength = "Le mot de passe doit contenir au moins 6 caractères.";
+        public const string InvalidUsernameLength = "Le nom d'utilisateur ne doit pas dépasser 20 caractères.";
 
     }
 }
