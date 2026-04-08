320d3f8bc5fc207dce28793c9aa98cca3390dc3e
Wilson Junior Farel Pierre <2380179@cegepmv.ca>
2026-04-08 13:07:48 -0400

    Rajout des fichiers requis

---
+++ Frontend/ViewModels/ChannelListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Frontend.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.ViewModels
+{
+    internal class ChannelListViewModel : BaseViewModel
+    {
+        public ChannelListViewModel() { }
+    }
+}
