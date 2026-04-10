077d4de6ce7eca16bb8a4ba43466f47038599b41
Wilson Junior Farel Pierre <2380179@cegepmv.ca>
2026-04-10 11:47:40 -0400

    Modification des logiques
    L'objet serveur est maintenant hérité de ServerViewModel

--- Frontend/ViewModels/ServerViewModel.cs
+++ Frontend/ViewModels/ServerViewModel.cs
@@ -1,17 +1,27 @@
-﻿using System;
+﻿using Frontend.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Frontend.ViewModels
 {
     public class ServerViewModel
     {
         public string Name { get; set; }
+
+        public ICommand? Test { get; }
         public ServerViewModel() 
         {
             Name = string.Empty;
+            Test = new RelayCommand(declenche, ()=> true);
+        }
+
+        public void declenche()
+        {
+            Console.WriteLine("Déclenché");
         }
     }
 }
