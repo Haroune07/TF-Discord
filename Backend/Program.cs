0dee52e0442a5cae46ba9fd17bfb19a1b41e54bd
Salah <2169461@cegepmv.ca>
2026-04-11 09:40:50 -0400

    ServerService créer et implementer

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -15,6 +15,9 @@ namespace Backend
             var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
             builder.Services.AddControllers();
+            builder.Services.AddScoped<ServerService>();
+            builder.Services.AddScoped<IRepository<Server>, MongoRepository<Server>>();
+            builder.Services.AddScoped<IRepository<ServerMember>, MongoRepository<ServerMember>>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
