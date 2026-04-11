fa684306953785cf094d7608207326706042655c
Carlof130 <2380179@cegepmv.ca>
2026-04-11 16:31:42 -0400

    Ajout et finalisation du AvatarControl
    Le AvatarControle fonctionne avec la vraie base de donnée

---
+++ Frontend/Views/Components/AvatarControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Shared.DTOs;
+
+namespace Frontend.Views.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour AvatarControl.xaml
+    /// </summary>
+    public partial class AvatarControl : UserControl
+    {
+        
+
+        public AvatarControl()
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.AvatarControlViewModel();
+        }
+    }
+}
