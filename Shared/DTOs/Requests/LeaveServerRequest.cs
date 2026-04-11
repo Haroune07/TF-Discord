fd17b12033505e325180368099327760251e307d
Haroune07 <2471001@cegepmv.ca>
2026-04-11 15:11:57 -0400

    réuni LeaveServer et JoinServer dans un seul dto pour réduire le nombre de classe

---
+++ Shared/DTOs/Requests/LeaveServerRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.Requests
+{
+    public class LeaveServerRequest
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string ServerId { get; set; } = string.Empty;
+    }
+}
