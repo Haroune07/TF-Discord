482f800e1a1633a45c5fd8bc4adb4f875cc9302e
HBelhachani <2471001@cegepmv.ca>
2026-04-10 23:09:17 -0400

    + méthodes apiService

--- Frontend/Services/ApiService.cs
+++ Frontend/Services/ApiService.cs
@@ -1,8 +1,9 @@
-﻿using Shared.DTOs.Auth;
+﻿using Shared.Constants;
+using Shared.DTOs;
+using Shared.DTOs.Auth;
+using Shared.DTOs.Requests;
 using System.Net.Http;
 using System.Net.Http.Json;
-using Shared.Constants;
-using Shared.DTOs.Requests;
 
 namespace Frontend.Services
 {
@@ -30,6 +31,51 @@ namespace Frontend.Services
             var res = await _client.PostAsJsonAsync(Routes.LoginRoute, req);
 
             return (await res.Content.ReadFromJsonAsync<AuthResponse>())!;
+        }
+
+        public async Task<List<ChannelDTO>> GetServerChannelsAsync(string serverId)
+        {
+            return await _client.GetFromJsonAsync<List<ChannelDTO>>($"{Routes.GetServerChannelsRoute}/{serverId}") ?? new();
+        }
+
+        public async Task<ChannelDTO?> CreateDMAsync(CreateDMRequest req)
+        {
+            var res = await _client.PostAsJsonAsync(Routes.CreateDMChannelRoute, req);
+            return res.IsSuccessStatusCode ? await res.Content.ReadFromJsonAsync<ChannelDTO>() : null;
+        }
+
+        public async Task<List<MessageDTO>> GetMessagesAsync(string channelId)
+        {
+            return await _client.GetFromJsonAsync<List<MessageDTO>>($"{Routes.GetChannelMessagesRoute}/channel/{channelId}") ?? new();
+        }
+
+        public async Task<MessageDTO?> SendMessageAsync(CreateMessageRequest req)
+        {
+            var res = await _client.PostAsJsonAsync(Routes.SendMessageRoute, req);
+            return res.IsSuccessStatusCode ? await res.Content.ReadFromJsonAsync<MessageDTO>() : null;
+        }
+
+        public async Task<List<ServerDTO>> GetMyServersAsync(string userId)
+        {
+            return await _client.GetFromJsonAsync<List<ServerDTO>>($"{Routes.GetMyServersRoute}/{userId}") ?? new();
+        }
+
+        public async Task<ServerDTO?> CreateServerAsync(CreateServerRequest req)
+        {
+            var res = await _client.PostAsJsonAsync(Routes.CreateServerRoute, req);
+            return res.IsSuccessStatusCode ? await res.Content.ReadFromJsonAsync<ServerDTO>() : null;
+        }
+
+        public async Task<bool> JoinServerAsync(JoinServerRequest req)
+        {
+            var res = await _client.PostAsJsonAsync(Routes.JoinServerRoute, req);
+            return res.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> LeaveServerAsync(JoinServerRequest req)
+        {
+            var res = await _client.PostAsJsonAsync(Routes.LeaveServerRoute, req);
+            return res.IsSuccessStatusCode;
         }
 
     }
