8fa3b27f543f0cf443c7ee6212ff8ac8b8c7a50f
Haroune07 <2471001@cegepmv.ca>
2026-04-11 12:57:19 -0400

    delete modèles redondants

--- Frontend/Models/FausseListes.cs
+++ Frontend/Models/FausseListes.cs
@@ -1,29 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using Shared.DTOs;
 
 namespace Frontend.Models
 {
     public static class MockServerData
     {
-        public static List<Server> GetFakeServers()
+        public static List<ServerDTO> GetFakeServers()
         {
-            return new List<Server>
+            return new List<ServerDTO>
             {
-                new Server { Id = 1, Name = "Serveur Europe"},
-                new Server { Id = 2, Name = "Serveur America"},
-                new Server { Id = 3, Name = "Serveur Asia"}
+                new ServerDTO { Id = "1", Name = "Serveur Europe"},
+                new ServerDTO { Id = "2", Name = "Serveur America"},
+                new ServerDTO { Id = "3", Name = "Serveur Asia"}
             };
         }
 
-        public static List<Channel> GetFakeChannels()
+        public static List<ChannelDTO> GetFakeChannels()
             {
-                return new List<Channel>
+                return new List<ChannelDTO>
             {
-                new Channel { Id = 1, ServerId = 1, Name = "📢-annonces" },
-                new Channel { Id = 2, ServerId = 1, Name = "💬-général" },
-                new Channel { Id = 3, ServerId = 1, Name = "🎮-gaming" },
-                new Channel { Id = 4, ServerId = 2, Name = "💻-dev-talk" },
-                new Channel { Id = 5, ServerId = 3, Name = "🤖-bots" }
+                new ChannelDTO { Id = "1", ServerId = "1", Name = "📢-annonces" },
+                new ChannelDTO { Id = "2", ServerId = "1", Name = "💬-général" },
+                new ChannelDTO { Id = "3", ServerId = "1", Name = "🎮-gaming" },
+                new ChannelDTO { Id = "4", ServerId = "2", Name = "💻-dev-talk" },
+                new ChannelDTO { Id = "5", ServerId = "3", Name = "🤖-bots" }
             };
             }
     }
