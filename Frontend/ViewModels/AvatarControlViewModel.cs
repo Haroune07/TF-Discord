e573b64fdeeed092f0359e0cefff7f40a9c2e54c
Haroune07 <2471001@cegepmv.ca>
2026-04-11 18:51:00 -0400

    rendu Avatar Control Réutilisable

--- Frontend/ViewModels/AvatarControlViewModel.cs
+++ Frontend/ViewModels/AvatarControlViewModel.cs
@@ -1,36 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Frontend.Global;
+﻿using Frontend.Global;
+using Frontend.ViewModels.Base;
+using Shared.DTOs;
 
 namespace Frontend.ViewModels
 {
-    public class AvatarControlViewModel
+    public class AvatarControlViewModel : BaseViewModel
     {
-        public string Initials { get; set; } = Session.Current.User.Username.Substring(0, 2).ToUpper();
+        public string Initials { get; set; } = string.Empty;
+        public string? AvatarImage { get; set; }
+        public double AvatarOpacity { get; set; }
+        public bool IsOnline { get; set; }
 
-        public string AvatarImage { get; set; } = Session.Current.User.ProfileImageUrl;
+        //le userDTO est passé en parametre ca nous permet de reutiliser aillerus (msg, dm, user status)
+        public AvatarControlViewModel(UserDTO? user = null)
+        {
+            user ??= Session.Current.User; // Si pas d'user fourni, on prend moi
 
-        public int AvatarOpacity { get; set; }
-
-        public AvatarControlViewModel() 
-        { 
-            if(AvatarImage == null)
+            if (user != null)
             {
-                AvatarOpacity = 0;
+                string name = user.Username ?? "??";
+                Initials = name.Length >= 2 ? name.Substring(0, 2).ToUpper() : name.ToUpper();
+                AvatarImage = user.ProfileImageUrl;
+                IsOnline = user.IsOnline;
+                AvatarOpacity = string.IsNullOrEmpty(AvatarImage) ? 0 : 1;
             }
-            else { AvatarOpacity = 1; }
         }
     }
 }
