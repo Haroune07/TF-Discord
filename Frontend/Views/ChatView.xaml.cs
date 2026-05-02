88cd4f116fc19fcf058de3d39b1a0079587e25af
Haroune07 <2471001@cegepmv.ca>
2026-05-02 16:35:24 -0400

    suppression message et modif avec succès

--- Frontend/Views/ChatView.xaml.cs
+++ Frontend/Views/ChatView.xaml.cs
@@ -1,15 +1,23 @@
-﻿using System.Windows.Controls;
+﻿using Frontend.ViewModels;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Frontend.Views
 {
-    /// <summary>
-    /// Interaction logic for ChatView.xaml
-    /// </summary>
     public partial class ChatView : UserControl
     {
         public ChatView()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape && DataContext is ChatViewModel vm && vm.IsEditing)
+            {
+                vm.CancelEditCommand.Execute(null);
+                e.Handled = true;
+            }
         }
     }
 }
