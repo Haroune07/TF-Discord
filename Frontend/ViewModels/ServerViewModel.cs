5f1fb52abc54662108a6d8cec2d081b41066155b
Salah <2169461@cegepmv.ca>
2026-05-03 22:17:07 -0400

    Sprint 3-DTF-62- feature:  - indicateur serveur actif sidebar

--- Frontend/ViewModels/ServerViewModel.cs
+++ Frontend/ViewModels/ServerViewModel.cs
@@ -4,13 +4,16 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace Frontend.ViewModels
 {
-    public class ServerViewModel : ObservableObject
+    public  partial class  ServerViewModel : ObservableObject
     {
         public string Name { get; set; }
         public string Id { get; set; }
         public string? ServerImageUrl { get; set; }
         public string Initials => Name.Length >= 2 ? Name.Substring(0, 2).ToUpper() : Name.ToUpper();
         public IRelayCommand SelectCommand { get; }
+
+        [ObservableProperty]
+        private bool isSelected;
 
         public ServerViewModel(string name, string id, Action<string> onSelected, string? serverImageUrl = null)
         {
