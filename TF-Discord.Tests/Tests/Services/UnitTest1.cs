9ebd5cc6e24ed816b6868f641ab1e9007fbb1d37
ddryad <6304455@cegepmv.ca>
2026-05-09 21:51:14 -0400

    base test project structure

---
+++ TF-Discord.Tests/Tests/Services/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TF_Discord.Tests.Tests.Services
+{
+    public class EmptyTest
+    {
+        [Fact]
+        public void Setup_TestProject_TestRun()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
